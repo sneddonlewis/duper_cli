1ff714477ee49abad88e93fa411334b30d90d5f6
Lewis Sneddon <lewis@lewiscodes.io>
2023-01-01 10:01:47 +0000

    Default path if not provided

--- src/bin/duper_cli.rs
+++ src/bin/duper_cli.rs
@@ -1,7 +1,10 @@
+use std::path::PathBuf;
 use clap::Parser;
 
 fn main() {
     println!("Duper");
     let opts = duper::opts::Opts::parse();
-    println!("{:?}", opts);
+    // replace with pwd
+    let path = opts.path.unwrap_or(PathBuf::from(r"~/CLionProjects/duper/test_data"));
+    println!("Path: {:?}", path);
 }
