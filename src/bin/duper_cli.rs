4e2fe43a46ba7bfca9051966c661e439fac06d78
Lewis Sneddon <lewis@lewiscodes.io>
2023-01-01 10:07:56 +0000

    Read contents of directory

--- src/bin/duper_cli.rs
+++ src/bin/duper_cli.rs
@@ -1,10 +1,14 @@
+use std::fs;
 use std::path::PathBuf;
 use clap::Parser;
 
 fn main() {
     println!("Duper");
     let opts = duper::opts::Opts::parse();
-    // replace with pwd
-    let path = opts.path.unwrap_or(PathBuf::from(r"~/CLionProjects/duper/test_data"));
+    let path = opts.path.unwrap_or(PathBuf::from(r"./test_data"));
     println!("Path: {:?}", path);
+
+    for file in fs::read_dir(path).unwrap() {
+        println!("{}", file.unwrap().path().display())
+    }
 }
