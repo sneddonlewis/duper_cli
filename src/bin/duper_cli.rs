5554b917cabcbd1b30307f8b05d4203ca738482f
Lewis Sneddon <lewis@lewiscodes.io>
2023-01-01 11:26:51 +0000

    Want to print file paths but problem with iterator ownership

--- src/bin/duper_cli.rs
+++ src/bin/duper_cli.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 use clap::Parser;
-use walkdir::WalkDir;
-use duper::found_file::FoundFile;
+use duper::found_file::new_file_list;
 
 fn main() {
     println!("Duper");
@@ -11,25 +10,6 @@ fn main() {
 
     let path = opts.path.unwrap_or(PathBuf::from(r"./test_data"));
 
-    // does not follow sym links
-    // walks hidden files
-
-    let mut files: Vec<FoundFile> = Vec::new();
-
-    for entry in WalkDir::new(path.as_path()).into_iter().filter_map(|e| e.ok()) {
-        if entry.path().is_dir() {
-            continue;
-        }
-        if extension_filter.is_some() {
-            // skip files without extension if there is a filter
-            if entry.path().extension().is_none() {
-                continue;
-            }
-        }
-        files.push(FoundFile{ path: entry.path().to_owned() })
-    }
-
-    for file in files.into_iter() {
-        println!("{:?}", file.path);
-    }
+    let file_list = new_file_list(path, extension_filter);
+    file_list.list_files();
 }
--- src/found_file.rs
+++ src/found_file.rs
@@ -1,5 +1,40 @@
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
-pub struct FoundFile {
-    pub path: PathBuf,
+struct FoundFile {
+    path: PathBuf,
+}
+
+pub struct FileList {
+    files: Vec<FoundFile>,
+}
+
+impl FileList {
+    pub fn list_files(&self) {
+        // for file in self.files.into_iter() {
+        //     println!("{:?}", file.path);
+        // }
+        println!("list files here");
+    }
+}
+
+pub fn new_file_list(base_path: PathBuf, extension_filter: Option<String>) -> FileList {
+    let mut files: Vec<FoundFile> = Vec::new();
+
+    for entry in WalkDir::new(base_path.as_path()).into_iter().filter_map(|e| e.ok()) {
+        if entry.path().is_dir() {
+            continue;
+        }
+        if extension_filter.is_some() {
+            // skip files without extension if there is a filter
+            if entry.path().extension().is_none() {
+                continue;
+            }
+        }
+        files.push(FoundFile { path: entry.path().to_owned() })
+    }
+
+    FileList {
+        files
+    }
 }
